2394efb29ec41287825c25e5daaa01e7e3139368
SoSweetHam <sosweetham@gmail.com>
2026-02-22 21:41:39 +0530

    feat: syner setup

---
+++ apps/syner/src-tauri/gen/android/app/src/main/java/is/syr/syner/MainActivity.kt
@@ -0,0 +1,11 @@
+package `is`.syr.syner
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
